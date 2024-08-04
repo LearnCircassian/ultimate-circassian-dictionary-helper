680add43f60a833a30adc6e3dc285efb92c716ad
Nart <113383731+nartna123@users.noreply.github.com>
2024-08-04 21:23:13 +0300

    .

--- convertOriginalFilesToJson/index.go
+++ convertOriginalFilesToJson/index.go
@@ -15,38 +15,42 @@ var (
 )
 
 func CallAllConverts() {
-	convert0()  // Ady-Ady_AIG.json
-	convert1()  // Ady-Ady_AP.json
-	convert2()  // Ady-Ara.json
-	convert3()  // Ady-En.json
-	convert4()  // Ady-En_Adam.json
-	convert5()  // Ady-Rus_Qarden.json
-	convert6()  // Ady-Rus_Sherdjes.json
-	convert7()  // Ady-Rus_Tharkaho.json
-	convert8()  // Ady-Tur_Huvaj.json
-	convert9()  // En-Ady.json
-	convert10() // En-Ady_Adam.json
-	convert11() // En-Kbd-Jonty.json
-	convert12() // En-Kbd-Ziwar.json
-	convert13() // Kbd-Ar-Jonty.json
-	convert14() // Kbd-En-2-Jonty.json
-	convert15() // Kbd-En-Jonty.json
-	convert16() // Kbd-En-Ziwar.json
-	convert17()
-	convert18()
-	convert19()
-	convert20()
-	convert21()
-	convert22()
-	convert23()
-	convert24()
-	convert25()
-	convert26()
-	convert27()
-	convert28()
-	convert29()
-	convert30()
-	convert31()
+	/*
+		convert0()  // Ady-Ady_AIG.json
+		convert1()  // Ady-Ady_AP.json
+		convert2()  // Ady-Ara.json
+		convert3()  // Ady-En.json
+		convert4()  // Ady-En_Adam.json
+		convert5()  // Ady-Rus_Qarden.json
+		convert6()  // Ady-Rus_Sherdjes.json
+		convert7()  // Ady-Rus_Tharkaho.json
+		convert8()  // Ady-Tur_Huvaj.json
+		convert9()  // En-Ady.json
+		convert10() // En-Ady_Adam.json
+		convert11() // En-Kbd-Jonty.json
+		convert12() // En-Kbd-Ziwar.json
+		convert13() // Kbd-Ar-Jonty.json
+		convert14() // Kbd-En-2-Jonty.json
+		convert15() // Kbd-En-Jonty.json
+		convert16() // Kbd-En-Ziwar.json
+		convert17()
+		convert18()
+		convert19()
+		convert20()
+		convert21()
+		convert22()
+		convert23()
+		convert24()
+		convert25()
+		convert26()
+		convert27()
+		convert28()
+		convert29()
+		convert30()
+		convert31()
+
+	*/
+	convert32()
 
 	// print all the words in Circassian
 	fmt.Printf("\n--All the words in Circassian--\n")
@@ -1860,6 +1864,69 @@ func convert31() {
 			value = utils.ConvertIToCirStick(value)
 			dictObj.Words[spelling].AddOneDefinition(value, []wordObject.Example{})
 		}
+	}
+
+	// print invalid lines
+	fmt.Printf("\n--Invalid lines in %s:--\n", srcFilePath)
+	for idx, line := range invalidLinesList {
+		fmt.Printf("%d. %s\n", idx, line)
+	}
+
+	utils.CreateFileWithDictionaryObject(distFilePath, dictObj)
+}
+
+// convert32() Rus-Kbd_Nalchik_2013.txt
+func convert32() {
+	dictObj := wordObject.NewDictionaryObject("Еджап1эм папщ1э урыс-адыгэ псалъалъэ (2013)", 32, "Ru", "Kbd", "JSON")
+	srcFilePath := "D:\\Github\\Cir\\ultimate-circassian-dictionary-helper\\srcDicts\\Rus-Kbd_Nalchik_2013.txt"
+	distFilePath := fmt.Sprintf("D:\\Github\\Cir\\ultimate-circassian-dictionary-helper\\distDicts\\%d.Rus-Kbd_Nalchik_2013.json", dictObj.Id)
+	invalidLinesList := make([]string, 0)
+	entireFileStr := utils.ReadEntireFile(srcFilePath)
+	lines := strings.Split(entireFileStr, "\n")
+
+	addNewLineAndTabToNumberDot := func(s string) string {
+		s = strings.ReplaceAll(s, " 1.", "\n\t1.")
+		s = strings.ReplaceAll(s, " 2.", "\n\t2.")
+		s = strings.ReplaceAll(s, " 3.", "\n\t3.")
+		s = strings.ReplaceAll(s, " 4.", "\n\t4.")
+		s = strings.ReplaceAll(s, " 5.", "\n\t5.")
+		s = strings.ReplaceAll(s, " 6.", "\n\t6.")
+		s = strings.ReplaceAll(s, " 7.", "\n\t7.")
+		s = strings.ReplaceAll(s, " 8.", "\n\t8.")
+		s = strings.ReplaceAll(s, " 9.", "\n\t9.")
+		s = strings.ReplaceAll(s, " 1)", "\n\t\t1)")
+		s = strings.ReplaceAll(s, " 2)", "\n\t\t2)")
+		s = strings.ReplaceAll(s, " 3)", "\n\t\t3)")
+		s = strings.ReplaceAll(s, " 4)", "\n\t\t4)")
+		s = strings.ReplaceAll(s, " 5)", "\n\t\t5)")
+		s = strings.ReplaceAll(s, " 6)", "\n\t\t6)")
+		s = strings.ReplaceAll(s, " 7)", "\n\t\t7)")
+		s = strings.ReplaceAll(s, " 8)", "\n\t\t8)")
+		s = strings.ReplaceAll(s, " 9)", "\n\t\t9)")
+		return s
+	}
+
+	for idx, line := range lines {
+		line = strings.Trim(line, " ")
+
+		// Split the line into words
+		words := strings.Fields(line)
+
+		if len(words) == 0 {
+			invalidLinesList = append(invalidLinesList, line)
+			continue
+		}
+
+		spelling := strings.ToLower(words[0])
+		value := addNewLineAndTabToNumberDot(line)
+		value = utils.ConvertIToCirStick(value)
+
+		if _, ok := dictObj.Words[spelling]; !ok {
+			dictObj.Words[spelling] = wordObject.NewWordObject(spelling, "")
+		}
+		dictObj.Words[spelling].AddOneDefinition(value, []wordObject.Example{})
+
+		fmt.Printf("line %d: %s\n", idx, line)
 	}
 
 	// print invalid lines
