abb81201b18970a107f1ea3ff90b0d719b498359
Nart <113383731+nartna123@users.noreply.github.com>
2024-08-05 21:31:03 +0300

    .

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"ultimate-circassian-dictionary-helper/convertHtmlToDb"
+	"ultimate-circassian-dictionary-helper/convertJsonToHtml"
 	"ultimate-circassian-dictionary-helper/convertOriginalFilesToJson"
 )
 
 func main() {
 	convertOriginalFilesToJson.CallAllConverts()
-	// convertJsonToHtml.CallAllConverts()
-	// convertHtmlToDb.CallAllConverts()
+	convertJsonToHtml.CallAllConverts()
+	convertHtmlToDb.CallAllConverts()
 }
